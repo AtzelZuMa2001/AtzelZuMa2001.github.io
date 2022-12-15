3a6bf3ffaf5b6e693603751f00d8c4659021fd14
AtzelZuMa2001 <axel30abr@gmail.com>
2022-12-15 03:31:55 -0600

    agregamos el proyecto
    :D

---
+++ script/mainScript.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+const botonLateral = document.getElementsByClassName(".botonLateral");
+ 
+//botonLateral.addEventListener('click',);
+function mostrar(){
+    document.getElementById("barraLateral").classList.toggle("active");
+}
+ 
